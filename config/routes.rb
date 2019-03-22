dbe49d038abf483b7d7e069c8366567ab95dd783
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-22 16:35:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 
-  get 'users/new'
+  resources :users, only: [:new, :create]
+
+  root 'welcome#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
