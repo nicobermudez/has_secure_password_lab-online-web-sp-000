74fef35f81e7afdb1fdfe0a0fda924e3bef1b2e4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-22 16:31:38 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,10 @@ class UsersController < ApplicationController
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
   end
+
+
+  private
+
+  def user_params
+    params.require(:user)
 end
